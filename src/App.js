c9b6037a33662ef2988dfec41e58a9a737e9df18
gab <gabrielfontes321@gmail.com>
2022-08-19 15:43:39 -0300

    count down app prototype

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+const COUNTDOWN_INITIAL_TIME_IN_SECONDS = 25 * 60;
+
 function App() {
+  const [secondsAmout, setSecondsAmount] = useState(COUNTDOWN_INITIAL_TIME_IN_SECONDS); 
+  const minutes = Math.floor(secondsAmout / 60);
+  const seconds = secondsAmout % 60;
+
+  useEffect(() => {
+    if (secondsAmout === 0) {
+      alert('Timer zerado')
+      return;
+    }
+
+    setTimeout(() => {
+      setSecondsAmount(state => state - 1);
+    }, 1000)
+  }, [secondsAmout])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <span>{String(minutes).padStart(2, '0')}</span>
+      <span>:</span>
+      <span>{String(seconds).padStart(2, '0')}</span>
     </div>
   );
 }
